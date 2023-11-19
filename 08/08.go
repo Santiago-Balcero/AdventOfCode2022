03755b2b72ea99b4c281ab860c38a9ece2b9f8f9
Santiago-Balcero <santiagobalcero@gmail.com>
2023-11-19 15:41:58 -0500

    Day 8: :star::star:

--- 08/08.go
+++ 08/08.go
@@ -30,8 +30,40 @@ func isVisible(treeLine *[]int, treePos *int) bool {
 			}
 		}
 	}
-	fmt.Printf("%v in position %v is visible? %v\n", (*treeLine)[*treePos], *treePos, visible)
+	fmt.Printf(
+		"%v in position %v is visible? %v\n",
+		(*treeLine)[*treePos],
+		*treePos,
+		visible,
+	)
 	return visible
+}
+
+func calculateScenicScore(treeLine *[]int, treePos *int) int {
+	visibleTreesA := 0
+	visibleTreesB := 0
+	// Right or top side of treeLine
+	for i := *treePos - 1; i >= 0; i-- {
+		visibleTreesA++
+		if (*treeLine)[i] >= (*treeLine)[*treePos] {
+			break
+		}
+	}
+	// Left or bottom side of treeLine
+	for i := *treePos + 1; i < len(*treeLine); i++ {
+		visibleTreesB++
+		if (*treeLine)[i] >= (*treeLine)[*treePos] {
+			break
+		}
+	}
+	fmt.Printf(
+		"Visible trees from %v in %v: %v - %v\n",
+		(*treeLine)[*treePos],
+		(*treeLine),
+		visibleTreesA,
+		visibleTreesB,
+	)
+	return visibleTreesA * visibleTreesB
 }
 
 func main() {
@@ -62,6 +94,7 @@ func main() {
 	}
 	file.Close()
 
+	// FIRST STAR
 	// Visible trees from edge
 	visibleTrees := lengthOfRow*2 + (len(forest)-2)*2
 
@@ -84,5 +117,29 @@ func main() {
 			}
 		}
 	}
-	fmt.Println(visibleTrees)
+	fmt.Println("NUMBER OF VISIBLE TREES:", visibleTrees)
+
+	// SECOND STAR
+	maxScenicScore := 0
+	for i := 1; i < len(forest)-1; i++ {
+		for j := 1; j < len(forest[i])-1; j++ {
+			// Scenic score in row
+			rowScore := calculateScenicScore(&forest[i], &j)
+			// Scenic score in column
+			treeColumn := []int{}
+			for i := 0; i < len(forest); i++ {
+				treeColumn = append(treeColumn, forest[i][j])
+			}
+			colScore := calculateScenicScore(&treeColumn, &i)
+			fmt.Printf(
+				"Scenic score of %v: %v\n",
+				forest[i][j],
+				rowScore*colScore,
+			)
+			if colScore*rowScore > maxScenicScore {
+				maxScenicScore = colScore * rowScore
+			}
+		}
+	}
+	fmt.Println("MAX SCENIC SCORE:", maxScenicScore)
 }
